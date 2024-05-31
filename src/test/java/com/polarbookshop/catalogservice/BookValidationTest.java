c185a9737752c8ac458e7b842b504868978cee21
Decamiron <vlad.krutik2002@gmail.com>
2024-05-31 19:55:42 +0300

    add tests class

---
+++ src/test/java/com/polarbookshop/catalogservice/BookValidationTest.java
@@ -0,0 +1,35 @@
+package com.polarbookshop.catalogservice;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class BookValidationTest {
+  private static Validator validator;
+
+  @BeforeAll
+  static void setUp() {
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    validator = factory.getValidator();
+  }
+
+  @Test
+  public void testAllFieldsAreValid() {
+    var book = new Book("1234567890", "Book 1", "Author 1", 9.90);
+    var result = validator.validate(book);
+    assertThat(result).isEmpty();
+  }
+
+  @Test
+  public void testNotAllFieldsAreValid() {
+    var book = new Book("12345678q90", "Book 1", "Author 1", 9.90);
+    var result = validator.validate(book);
+    assertThat(result).hasSize(1);
+    assertThat(result.iterator().next().getMessage()).isEqualTo("The ISBN format must be valid.");
+  }
+}
